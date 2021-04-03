1bfad5caf1be8772629206fd05a8435a52a254eb
oneCiser <41345659+oneCiser@users.noreply.github.com>
2021-04-03 13:23:49 -0500

    fix(stream): stream route
    fix stream route

--- src/routes/stream.route.ts
+++ src/routes/stream.route.ts
@@ -21,7 +21,7 @@ class ExampleRouter  {
 
   createRoutes(): void {
     this.router.get(
-      `/loadvideo/:id/:ud`,
+      `/:id/:ud`,
       // passport.authenticate('jwt',{session:false}),
       (req: Request, res: Response, next: NextFunction) => StreamVideoController
       .loadVideo(req, res, next)
